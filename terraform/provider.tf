993904428b17bf636925ea3443df419707a9c02e
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-12-09 21:00:52 -0300

    feat: setup terraform aws provider

---
+++ terraform/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+
+  backend "s3" {
+    bucket  = "terraform-tfstate"
+    key     = "kubernetes/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
