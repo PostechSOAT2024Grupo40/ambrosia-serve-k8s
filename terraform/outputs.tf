31906e19b984f131be585db7e5e1f9961b653cc5
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-12-14 14:22:17 -0300

    feat: create ecr

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,16 @@ output "cluster_endpoint" {
   value       = aws_eks_cluster.cluster.endpoint
   description = "The endpoint for the EKS cluster"
 }
+
+output "ecr_repository_url" {
+  value       = aws_ecr_repository.ecr.repository_url
+  description = "The URL of the ECR repository"
+}
+
+output "ecr_repository_id" {
+  value = aws_ecr_repository.ecr.registry_id
+}
+
+output "ecr_repository_arn" {
+  value = aws_ecr_repository.ecr.arn
+}
