4cf2e96275b871a7c5ffd6da92d6275f95b5e068
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-12-17 07:36:00 -0300

    feat: create alb

---
+++ terraform/alb.tf
@@ -0,0 +1,48 @@
+resource "aws_lb" "alb" {
+  name                       = "production-alb"
+  internal                   = true
+  load_balancer_type         = "application"
+  security_groups = [aws_security_group.sg.id]
+  subnets = [aws_subnet.subnet_private_a, aws_subnet.subnet_private_b]
+  idle_timeout               = 60
+  enable_deletion_protection = false
+
+
+  tags = {
+    Name = "production-alb"
+  }
+}
+
+
+resource "aws_lb_listener" "listener" {
+  depends_on = [aws_lb.alb]
+  load_balancer_arn = aws_alb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name        = "TG-ambrosia-serve"
+  port        = 80
+  target_type = "instance"
+  protocol    = "HTTP"
+
+  vpc_id = aws_vpc.ambrosia_serve_vpc.id
+
+  health_check {
+    path    = "/health"
+    port    = 80
+    matcher = "200"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "attach" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = data.aws_instance.ec2.id
+  port             = 80
+}
--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "sg" {
   depends_on  = [aws_vpc.ambrosia_serve_vpc]
   name        = "ambrosia-serve-sg"
-  description = "Allow HTTP port 8000 and all outbound traffic"
+  description = "Allow HTTP port 80 and all outbound traffic"
   vpc_id      = aws_vpc.ambrosia_serve_vpc.id
 
   tags = {
@@ -12,7 +12,7 @@ resource "aws_security_group" "sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4_80" {
   security_group_id = aws_security_group.sg.id
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = aws_vpc.ambrosia_serve_vpc.cidr_block
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
